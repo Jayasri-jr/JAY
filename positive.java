a8cc385c6bd9766057641a9b5bbde03e399e54a5
Jayasri-jr <47377689+Jayasri-jr@users.noreply.github.com>
2019-02-06 12:14:25 +0530

    Update positive.java

--- positive.java
+++ positive.java
@@ -1,19 +1,19 @@
-public class Demo
-{
-    public static void main(String[] args) 
-    {
-        int number=3;
-        if(number > 0)
-        {
-            System.out.println(number+" is a positive number");
-        }
-        else if(number < 0)
-        {
-            System.out.println(number+" is a negative number");
-        }
+public class PositiveNegative {
+
+    public static void main(String[] args) {
+
+        int number = 3;
+
+        // true if number is less than 0
+        if (number < 0)
+            System.out.println(number + "  negative");
+
+        // true if number is greater than 0
+        else if ( number > 0)
+            System.out.println(number + "  positive ");
+
+        // if both test expression is evaluated to false
         else
-        {
-            System.out.println(number+" is neither positive nor negative");
-        }
+            System.out.println(number + "  zero");
     }
 }
