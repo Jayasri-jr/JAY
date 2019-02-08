c2d36341f2ca43817b15e24bd833c4ba842e439a
Jayasri-jr <47377689+Jayasri-jr@users.noreply.github.com>
2019-02-08 15:44:34 +0530

    Create leap.java

---
+++ leap.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+ class leapornot{
+ 
+    public static void main(String[] args) {
+     int year;
+    	Scanner scan = new Scanner(System.in);
+    
+    	year = scan.nextInt();
+        
+        if(year%400==0)
+        {
+            System.out.println("Leap Year");
+        }
+        else if(year%4==0 && year%100!=0)
+        {
+            System.out.println("yes");
+        }
+        else
+        {
+            System.out.println("no");
+        }
+      
