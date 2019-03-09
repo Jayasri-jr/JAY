5dc1628b3efdd183845ed148066292da21b90c36
Jayasri-jr <47377689+Jayasri-jr@users.noreply.github.com>
2019-03-09 16:16:17 +0530

    Create comevenposit.java

---
+++ comevenposit.java
@@ -0,0 +1,28 @@
+
+27 lines (26 sloc) 475 Bytes
+import java.util.*;
+import java.lang.*;
+class comevenposit
+{
+   public static void main(String args[])
+   {
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        int flag=0;
+       for(int i=2;i<num;i++)
+       {
+           if(num%i==0)
+           {
+               flag=1;
+           }
+       }
+        if(flag==1)
+        {
+            System.out.print("yes");
+        }
+        else
+        {
+            System.out.print("no");
+        }
+   }
+} 
