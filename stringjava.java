3c06d3e86e1a3ce09090d3848d02b29e4d031fc3
Jayasri-jr <47377689+Jayasri-jr@users.noreply.github.com>
2019-03-09 11:48:51 +0530

    Create stringjava.java

---
+++ stringjava.java
@@ -0,0 +1,12 @@
+
+11 lines (10 sloc) 239 Bytes
+import java.util.Scanner;
+class string{ 
+public static void main(String[] args)
+  {
+    Scanner sc = new Scanner(System.in);
+    String str= sc.nextLine();
+     String array[]= str.split(" ",0);
+    System.out.println(array.length);
+  }
+}
