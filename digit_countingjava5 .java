72a400105f5915a97156320f2b6f5865811a20ac
Jayasri-jr <47377689+Jayasri-jr@users.noreply.github.com>
2019-03-13 16:33:15 +0530

    Create  digit_countingjava5 .java

---
+++ digit_countingjava5 .java
@@ -0,0 +1,15 @@
+import java.util.*;
+ class digit_countingjava5 
+ {
+    public static void main(String args[]){
+    Scanner input =new Scanner(System.in);
+    float n1,n2;
+    double i; 
+	n1=input.nextFloat(); 
+    n2=input.nextFloat();
+
+	 i=n1*n2;
+     System.out.format("%.5f",i);
+    }
+ 
+}
